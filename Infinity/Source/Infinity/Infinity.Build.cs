e7a6d51ae35cd78fb555f859ae9d66891bfd0a42
samuelmoores <samueldmoores@gmail.com>
2023-06-15 16:19:30 -0700

    Version 1.0

---
+++ Infinity/Source/Infinity/Infinity.Build.cs
@@ -0,0 +1,13 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+using UnrealBuildTool;
+
+public class Infinity : ModuleRules
+{
+	public Infinity(ReadOnlyTargetRules Target) : base(Target)
+	{
+		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
+
+		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "HeadMountedDisplay", "EnhancedInput" });
+	}
+}
